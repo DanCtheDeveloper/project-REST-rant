39843a7f3272c9580ce1f7365f1f8faa143473e0
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-06-21 11:35:37 -0700

    Comments update and Delete

--- views/default.jsx
+++ views/default.jsx
@@ -7,7 +7,7 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>RestRant!</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/css/style.css"/>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossOrigin="anonymous"
